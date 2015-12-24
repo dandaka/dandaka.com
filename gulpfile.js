c145f51c595c9b6dc1aa78e3bb8d089d2060cea8
Vlad Rafeev <vlad19@gmail.com>
2015-12-24 14:09:36 +0200

    Jade compilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
 var gulp = require('gulp');
+var jade = require('gulp-jade');
 
 gulp.task('default', function() {
   // place code for your default task here
+});
+
+gulp.task('jade', function() {
+  var YOUR_LOCALS = {};
+ 
+  gulp.src('./src/jade/*.jade')
+    .pipe(jade({
+      locals: YOUR_LOCALS
+    }))
+    .pipe(gulp.dest('./dist/'))
 });
