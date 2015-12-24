2ec0fd6d95a0fd88e97de37bcd9d6b564b86e6e4
Vlad Rafeev <vlad19@gmail.com>
2015-12-24 14:13:17 +0200

    default task uses jade

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,7 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
 
-gulp.task('default', function() {
-  // place code for your default task here
-});
+gulp.task('default', ['jade']);
 
 gulp.task('jade', function() {
   var YOUR_LOCALS = {};
