3f9d0abd92b19454c0b645fb6d5a754cc3a452fb
Vlad Rafeev <vlad19@gmail.com>
2015-12-24 17:40:03 +0200

    watch over changes in sass and jade

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 var gulp = require('gulp');
 var jade = require('gulp-jade');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var connect = require('gulp-connect');
 
-gulp.task('default', ['jade']);
+gulp.task('default', ['jade', 'sass', 'connect', 'watch']);
 
 gulp.task('jade', function() {
   var YOUR_LOCALS = {};
@@ -10,5 +13,28 @@ gulp.task('jade', function() {
     .pipe(jade({
       locals: YOUR_LOCALS
     }))
-    .pipe(gulp.dest('./dist/'))
+    .pipe(gulp.dest('./public/'))
+});
+
+gulp.task('sass', function () {
+  gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    root: 'public',
+    livereload: true
+  });
+});
+
+gulp.task('html', function () {
+  gulp.src('./public/*.html')
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['./src/jade/*.jade'], ['jade']);
+  gulp.watch(['./src/sass/*.scss'], ['sass']);
 });
